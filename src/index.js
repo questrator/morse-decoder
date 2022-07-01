86abc4fae86801599649ef6f11fa083ceb17743e
Slava Ivanov <drumtrack@ya.ru>
2022-07-01 15:37:25 +0300

    upd: done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const chunk = (s, n) => s.split("").reduce((r, e, i, a) => (i % n ? r : [...r, a.slice(i, i + n)]), []).map(e => e.join(""));
+    return chunk(expr, 10).map(e => chunk(e, 2).map(b => ({[00]:"",[10]:".",[11]:"-"})[b]).join("")).map(e => MORSE_TABLE[e] ?? " ").join("");
 }
 
 module.exports = {
     decode
 }
+
+/* chunk() implementation below is faster and easier to be read
+function chunk(string, n) {
+    const out = [];
+    for (let i = 0; i < string.length; i += n) {
+      out.push(string.slice(i, i + n));
+    }
+    return out;
+} */
